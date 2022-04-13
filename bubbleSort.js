0c06b358dd5bd064d4243e0b8e502a707a0310e3
Mayur G <mayurgare1@gmail.com>
2022-04-13 20:29:59 +0530

    Code_Refactoring

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,24 +1,24 @@
-async function bubble() {
-    console.log('In bubbe()');
-    const ele = document.querySelectorAll(".block");
-    for(let i = 0; i < ele.length-1; i++){
-        console.log('In ith loop');
-        for(let j = 0; j < ele.length-i-1; j++){
-            console.log('In jth loop');
-            ele[j].style.background = 'blue';
-            ele[j+1].style.background = 'blue';
-            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
-                console.log('In if condition');
+const bubble = async () => {
+    const ele = document.querySelectorAll(`.block`);
+
+    for (let i = 0; i < ele.length-1; i++) {
+        
+        for (let j = 0; j < ele.length-i-1; j++) {
+            ele[j].style.background = `blue`;
+            ele[j+1].style.background = `blue`;
+
+            if (parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)) {
                 await sleep(delay);
                 swap(ele[j], ele[j+1]);
             }
-            ele[j].style.background = 'cyan';
-            ele[j+1].style.background = 'cyan';
+
+            ele[j].style.background = `cyan`;
+            ele[j+1].style.background = `cyan`;
         }
-        ele[ele.length-1-i].style.background = 'green';
+        ele[ele.length-1-i].style.background = `green`;
     }
-    ele[0].style.background = 'green';
+    ele[0].style.background = `green`;
 }
-document.getElementById("bubble").addEventListener("click",function(){bubble()})
+document.getElementById(`bubble`).addEventListener(`click`, () => { bubble() })
 
 
--- index.js
+++ index.js
@@ -3,31 +3,30 @@
 
 // Global Vars
 let screenWidth = window.innerWidth;
-let container = document.getElementById("array")
-let outerContainer = document.querySelector('.center');
+let container = document.getElementById(`array`)
+let outerContainer = document.querySelector(`.center`);
 let width, noOfBars, padding;
 
 let arrayOfBars = [];
 
 //Function to generate random arrays and display them
-function generatearray(noOfBars=100)
-{
+const generatearray = (noOfBars=100) => {
 	arrayOfBars = [];
 
 	for (let i = 0; i < noOfBars; i++)
 	{
 		let value = Math.ceil(Math.random() * 100);
-		let array= document.createElement("div");
-		array.classList.add("block");
+		let array= document.createElement(`div`);
+		array.classList.add(`block`);
 		array.style.height = `${value * 4}px`;
 
 		arrayOfBars.push(value * 4);
 
 		array.style.transform = `translate(${i * 10}px)`;
 		padding = (screenWidth - (noOfBars * 10)) / 2;
-		// array.appendChild(<span class="invisible">${ value }</span>)
+		// array.appendChild(<span class=`invisible`>${ value }</span>)
 		container.appendChild(array);
-		document.querySelector(".center").style.padding = `0 ${ padding }px`;
+		document.querySelector(`.center`).style.padding = `0 ${ padding }px`;
 	}
 	// console.log(arrayOfBars);
 }
@@ -36,48 +35,46 @@ const generateSorted = (arr) => {
 	removeBars()
 
 	arr.map((element, index) => {
-		let array= document.createElement("div");
-		array.classList.add("block");
+		let array= document.createElement(`div`);
+		array.classList.add(`block`);
 		array.style.height = `${element}px`;
 
 		array.style.transform = `translate(${index * 10}px)`;
-		// array.appendChild(<span class="invisible">${ value }</span>)
+		// array.appendChild(<span class=`invisible`>${ value }</span>)
 		container.appendChild(array);
 	})
 	padding = (screenWidth - (arr.length * 10)) / 2;
-	document.querySelector(".center").style.padding = `0 ${ padding }px`;
+	document.querySelector(`.center`).style.padding = `0 ${ padding }px`;
 
 }
 
 generatearray();
 
 //Function to remove the bars from the screen
-function removeBars()
-{
-    document.querySelectorAll(".block").forEach((node) =>
+const removeBars = () => {
+    document.querySelectorAll(`.block`).forEach((node) =>
 	{
         node.remove();
     });
 }
 
-//Linking the "New array" to the above funnction to generate random arrays on click
-document.getElementById("new").addEventListener("click", () => {
+//Linking the `New array` to the above funnction to generate random arrays on click
+document.getElementById(`new`).addEventListener(`click`, () => {
     removeBars();
    	generatearray();
 });
 
-//Scaling the array size according to the "Number of bars" slider
-let arraySize = document.querySelector('#number');
-arraySize.addEventListener('input', function()
-{
+//Scaling the array size according to the `Number of bars` slider
+let arraySize = document.querySelector(`#number`);
+arraySize.addEventListener(`input`, () => {
 	noOfBars = parseInt(arraySize.value)
     removeBars();
 	generatearray(parseInt(arraySize.value));
 });
 
 //function to swap bars
-function swap(el1, el2) {
-    console.log('In swap()');
+const swap = (el1, el2) => {
+    console.log(`In swap()`);
     
     let temp = el1.style.height;
     el1.style.height = el2.style.height;
@@ -85,15 +82,15 @@ function swap(el1, el2) {
     
 }
 
-function sleep(millisec) { 
+const sleep = (millisec) => { 
     return new Promise(resolve => { 
-        setTimeout(() => { resolve('') }, millisec); 
+        setTimeout(() => { resolve(``) }, millisec); 
     }) 
 }
 
 // Event listener to update delay time
 delay = 700 - 20
-let delayTime = document.querySelector('#speed');
-delayTime.addEventListener('input', function(){
+let delayTime = document.querySelector(`#speed`);
+delayTime.addEventListener(`input`, () => {
     delay = parseInt(700 - delayTime.value);
 });
